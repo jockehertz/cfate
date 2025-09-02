91ab8da9ca2c503b3f57c13ad4329c96376bc586
Joakim Hertzberg <joakim.d.hertzberg@gmail.com>
2025-09-02 21:05:57 +0200

    Completed lab 1

--- courses/dd1333/labs/1/calculate-planet-mass.py
+++ courses/dd1333/labs/1/calculate-planet-mass.py
@@ -5,6 +5,6 @@ def calculate_planet_mass(t, r):
     planet_mass = (4*(pi**2)*(r**3))/(G*(t**2))
     return planet_mass
 
-time = float(input("The time it takes for the satellite to orbit (hours): "))
-radius = float(input("The distance from the planet to (1000km): "))
+time = float(input("The time it takes for the satellite to orbit (hours): "))*(60**2)
+radius = float(input("The distance from the planet to (1000km): "))*(10**6)
 print(f"The planet weighs {str(calculate_planet_mass(time, radius))} Kg.")
